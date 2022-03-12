2af09fdd89e0171e6cb75d260d6163bac990824c
Christian Francini Mascagni <59649715+Chri2187@users.noreply.github.com>
2022-03-12 19:38:55 +0100

    minor update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Modal from "./Modal";
+
 function App() {
-  return <div className="App"></div>;
+  return <div>SideBar</div>;
 }
 
 export default App;
