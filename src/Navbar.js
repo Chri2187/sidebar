9a4ad1bc8ac74565e88b62ecb6a0800e27de4ceb
Christian Francini Mascagni <59649715+Chri2187@users.noreply.github.com>
2022-03-22 20:45:53 +0100

    Major Update
    Creati componenti Sidebar e Modal con relativi state

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,13 +3,14 @@ import { FaBars } from "react-icons/fa";
 import { links, SocialBar } from "./links";
 import { useGlobalContext } from "./context";
 const Navbar = () => {
+  const { openSidebar } = useGlobalContext();
   return (
     <nav className="nav">
       <header className="nav-header">
         <div className="nav-brand">
           <h4>Navbar</h4>
         </div>
-        <button className="btn nav-toggler">
+        <button className="btn nav-toggler" onClick={openSidebar}>
           <FaBars className="nav-icon" />
         </button>
       </header>
--- src/context.js
+++ src/context.js
@@ -3,8 +3,28 @@ import React, { useState, useContext } from "react";
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openSidebar = () => setIsSidebarOpen(true);
+  const closeSidebar = () => setIsSidebarOpen(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
   return (
-    <AppContext.Provider value={"Ciao ciao"}>{children}</AppContext.Provider>
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        closeSidebar,
+        openSidebar,
+        isModalOpen,
+        openModal,
+        closeModal,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
   );
 };
 
